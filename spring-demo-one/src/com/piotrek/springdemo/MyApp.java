5966956977408d7b102ec9d26aac02116b27ffbd
Piotrekf3 <piotrek.fur@wp.pl>
2019-03-13 22:59:36 +0100

    initial commit

---
+++ spring-demo-one/src/com/piotrek/springdemo/MyApp.java
@@ -0,0 +1,8 @@
+package com.piotrek.springdemo;
+
+public class MyApp {
+    public static void main(String[] args) {
+        Coach theCoach = new BaseballCoach();
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
