ec94c784fdf315d35e9b7ce411dbd7d5344eb813
Piotrekf3 <piotrek.fur@wp.pl>
2019-03-19 20:19:32 +0100

    Bean scope and lifecycle

--- spring-demo-one/src/com/piotrek/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/piotrek/springdemo/TrackCoach.java
@@ -21,4 +21,12 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just Do It: " + fortuneService.getFortune();
     }
+
+    public void doMyStartupStuff() {
+        System.out.println("Startup stuff");
+    }
+
+    public void doMyCleanupStuff() {
+        System.out.println("Cleanup stuff");
+    }
 }
